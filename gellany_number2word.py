0634af8c10291d3506e082b97b5fc8568001bda5
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-22 06:37:07 +0200

    Add files via upload

---
+++ gellany_number2word.py
@@ -0,0 +1,24 @@
+
+import num2words as n2w
+from tkinter import *
+
+class gellany_number2word():
+
+     def __init__(self, n = None):
+              self.n = n
+
+    
+
+     def main(self):
+
+                 arr = ['zero','one','two','three','four','five','six','seven','eight','nine']
+                 
+
+                 for i in self.n :
+                       print(arr[int(i)], end =" ")
+
+n = input()
+#print(n)
+print("Number Entered was : ", n)
+print("Converted to word it becomes: ",end="")
+gellany_number2word(n = n).main()
